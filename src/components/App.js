0ae2993d135500697a00daa341f6e1d3916ee30e
Kamil Olber <olb.kamil@gmail.com>
2019-02-09 11:58:39 +0100

    3.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,49 @@
 import React, { Component } from 'react';
-import './app.css';
-import { connect } from 'react-redux';
 
-const actionInc = { type: 'INCREMENT'};
- const actionDec = { type: 'DECREMENT'};
+import { connect } from 'react-redux'
 
+const actionInc = { type: 'INCREMENT' };
+
+const createActionInc = (value = 0) => ({
+  type: 'INCREMENT',
+  payload: {
+    value
+  },
+})
+// const actionDec = { type: 'DECREMENT' };
 
 class App extends Component {
   render() {
-    const { reduxStore, increment, decrement } = this.props;
+    const { reduxStore, increment } = this.props;
 
-    return <div className="app">
-      <div>App</div>  
-      <div>{reduxStore}</div>       
-    <div className="btnn">
-      <div onClick={()=>increment(3)}>+3</div>  
-      <div onClick={()=>increment(5)}>+5</div>  
-      <div onClick={()=>increment(7)}>+7</div>  
-      </div>
-      <div> ----- </div>
-      <div onClick={decrement}>-</div>  
-      </div>
+    return <div>
+      <div>App</div>
+      <div>{reduxStore}</div>
+
+      <div onClick={() => {
+        increment(3)
+      }}>+3</div>
+      <div onClick={() => {
+        increment(5)
+      }}>+5</div>
+      <div onClick={() => {
+        increment(7)
+      }}>+7</div>
+    </div>
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    reduxStore: state,
-  }
-};
+const mapStateToProps = (state) => ({
+  reduxStore: state,
+});
 
 const mapDispatchToProps = (dispatch) => ({
-  increment: (num) => {
-    console.log(num)
-    dispatch({...actionInc, num})
+  increment: (value) => {
+    dispatch(createActionInc(value));
   },
-  decrement: () => {
-    dispatch(actionDec)
-  },
-}) ;
+});
 
 export default connect(
-      mapStateToProps, 
-      mapDispatchToProps
-    )(App);
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
 import './index.css';
-import App from './components/App';
+import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
 
-function reducer(state = 0, action) {
-    console.log(action)
-    switch (action.type) {
-      case 'INCREMENT':
-        return state + action.num;
-      case 'DECREMENT':
-        return state - 1;
-      default:
-        return state;
-    }
+function reducer(state = 10, action) {
+
+  const { type, payload: { value } = {} } = action
+
+  switch (type) {
+    case 'INCREMENT':
+      return state + value;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
   }
+}
+
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}> 
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-    </Provider>, 
-    document.getElementById('root')
-    );
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
